48b6aa8cc19991b27b0977a6a20f7605ca5c1b20
Ali <59686248+Alyster27@users.noreply.github.com>
2021-12-26 21:23:18 +0100

    Commit my ngx-translate test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translation-demo';
+  language = 'fr';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang(this.language);
+  }
+
+  changeLanguage(): void {
+    if(this.language === 'fr') {
+    this.language = 'en';
+    } else {
+      this.language = 'fr';
+    }
+    this.translate.use(this.language)
+  }
+
 }
